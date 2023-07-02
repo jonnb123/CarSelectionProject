41e6a4eec6f7b74d0073a9abbff260f8a40dbfe6
Jonathan Bentham <jon104@hotmail.co.uk>
2023-07-02 17:03:02 +0100

    more assets and code

---
+++ Source/CarSelectionProject/CarSelectionProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CarSelectionProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARSELECTIONPROJECT_API ACarSelectionProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
